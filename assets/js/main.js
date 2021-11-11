f18055ac470956c79181a1f9cf4661d3509d6eff
鯖の禊 <80382375+mackerel-misogi@users.noreply.github.com>
2021-11-12 01:53:49 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-
+$(function(){
+    var headerHeight = 66;
+    $('a[href^="#"]').click(function(){
+        var href = $(this).attr("href");
+        var target = $(href);
+        var position = target.offset().top - headerHeight;
+        $('body,html').animate({scrollTop: position}, 250, 'swing');
+    });
+    
+    $(".navi02 li a").on('click',function(event){
+        $("#toggle").prop("checked",false);
+    });
+});
