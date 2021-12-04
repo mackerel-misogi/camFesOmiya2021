69d6229ea1bb745c64a0aaa023cd463fae5b0ce9
鯖の禊 <80382375+mackerel-misogi@users.noreply.github.com>
2021-12-05 06:31:52 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,25 @@ $(function(){
         $("#toggle").prop("checked",false);
     });
 });
+//アコーディオンをクリックした時の動作
+$(document).on('click','.accordion_bar', function() {
+  var findElm = $(this).prev(".box");
+  $(findElm).slideToggle();
+    
+  if($(this).hasClass('close')){
+    $(this).removeClass('close');
+  }else{
+    $(this).addClass('close');
+  }
+});
+
+$(document).on('click','.answer_bar', function() {
+  var findElm = $(this).prev(".gallery_work_image_answer");
+  $(findElm).slideToggle();
+    
+  if($(this).hasClass('close')){
+    $(this).removeClass('close');
+  }else{
+    $(this).addClass('close');
+  }
+});
